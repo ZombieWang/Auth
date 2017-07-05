daeccd68c60b9542f681bfa34aed7669422c3677
Frank <frank7300228@gmail.com>
2017-07-05 19:10:00 +0800

    finish session 58

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import firebase from 'firebase'
 import { Header } from './components/common'
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyCB5uvW48Wn_pgBF3i8CR-eo_vYR4HzHFo',
+            authDomain: 'auth-fbe73.firebaseapp.com',
+            databaseURL: 'https://auth-fbe73.firebaseio.com',
+            projectId: 'auth-fbe73',
+            storageBucket: 'auth-fbe73.appspot.com',
+            messagingSenderId: '818366531692'
+        })
+    }
+
     render() {
         return (
             <View>
