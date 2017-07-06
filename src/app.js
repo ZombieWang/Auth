9da49a4e427593e167152f510a4520cea206df20
Frank <frank7300228@gmail.com>
2017-07-06 21:06:26 +0800

    finish session 75

--- src/app.js
+++ src/app.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react'
 import { View } from 'react-native'
 import firebase from 'firebase'
 import FirebaseCredential from './FirebaseCredential'
-import { Header } from './components/common'
+import { Header, Card, CardSection, Button, Spinner } from './components/common'
 import LoginForm from './components/LoginForm'
 
 class App extends Component {
-    state = { loggedIn: false }
+    state = { loggedIn: null }
 
     componentWillMount() {
         firebase.initializeApp(FirebaseCredential)
 
         firebase.auth().onAuthStateChanged((user) => {
-            if(user) {
+            if (user) {
                 this.setState({ loggedIn: true })
             } else {
                 this.setState({ loggedIn: false })
@@ -20,11 +20,36 @@ class App extends Component {
         })
     }
 
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <Card>
+                        <CardSection>
+                            <Button>
+                                Log Out
+                            </Button>
+                        </CardSection>
+                    </Card>
+                )
+            case false:
+                return <LoginForm/>
+            default:
+                return (
+                    <Card>
+                        <CardSection>
+                            <Spinner size='large' />
+                        </CardSection>
+                    </Card>
+                )
+        }
+    }
+
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm/>
+                {this.renderContent()}
             </View>
         )
     }
