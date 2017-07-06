d60d39caba47aeb79978aa8700d7f61b52796b31
Frank <frank7300228@gmail.com>
2017-07-06 20:11:19 +0800

    finish session 70

--- src/app.js
+++ src/app.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
 import firebase from 'firebase'
+import FirebaseCredential from './FirebaseCredential'
 import { Header } from './components/common'
 import LoginForm from './components/LoginForm'
 
 class App extends Component {
     componentWillMount() {
-        firebase.initializeApp({
-            apiKey: 'AIzaSyCB5uvW48Wn_pgBF3i8CR-eo_vYR4HzHFo',
-            authDomain: 'auth-fbe73.firebaseapp.com',
-            databaseURL: 'https://auth-fbe73.firebaseio.com',
-            projectId: 'auth-fbe73',
-            storageBucket: 'auth-fbe73.appspot.com',
-            messagingSenderId: '818366531692'
-        })
+        firebase.initializeApp(FirebaseCredential)
     }
 
     render() {
